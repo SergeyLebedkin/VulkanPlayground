b6e1f3e40bdec4b1fa59bac55229e60dacd713cc
Sergii Liebodkin <s.lebedkin@gmail.com>
2019-02-03 13:17:28 +0200

    vma added

--- build/msvs2017/apps/vulkan_glfw_app/vulkan_glfw_app.cpp
+++ build/msvs2017/apps/vulkan_glfw_app/vulkan_glfw_app.cpp
@@ -1,10 +1,13 @@
-#include <vulkan/vulkan.h>
+//#include <vulkan/vulkan.h>
+#include <VmaUsage.h>
 #include <GLFW/glfw3.h>
+#include <glm/vec4.hpp>
 
 int main(void)
 {
 	// init GLFW
 	glfwInit();
+	if (!glfwVulkanSupported()) assert(0);
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
 

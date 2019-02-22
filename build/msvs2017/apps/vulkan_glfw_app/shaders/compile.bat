119ed316800a404f9c2807b1ed37e48616df8026
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2019-02-22 15:22:22 +0200

    glslangValidator move to tools folder

--- build/msvs2017/apps/vulkan_glfw_app/shaders/compile.bat
+++
@@ -1,5 +0,0 @@
-del *.spv
-glslangValidator.exe -V base.vert.glsl -o base.vert.spv
-glslangValidator.exe -V base.frag.glsl -o base.frag.spv
-glslangValidator.exe -V obj.vert.glsl -o obj.vert.spv
-glslangValidator.exe -V obj.frag.glsl -o obj.frag.spv
